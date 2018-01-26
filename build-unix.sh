fd936496f8a0801bcaddadba4fc2a8f29715c332
Dieter <lucien.zerger@gmail.com>
2018-01-27 00:54:04 +1100

    Updated unix build script.

--- build-unix.sh
+++ build-unix.sh
@@ -10,3 +10,14 @@ apt-get install -y build-essential libtool autotools-dev automake pkg-config lib
 ./configure
 make
 make install
+
+# collect binaries
+mkdir -p ./bin/linux
+cp -v ./src/strayacoind ./bin/linux/
+cp -v ./src/strayacoin-cli ./bin/linux/
+cp -v ./src/strayacoin-tx ./bin/linux/
+cp -v ./src/test/test_strayacoin ./bin/linux/
+cp -v ./src/bench/bench_strayacoin ./bin/linux/
+cp -v ./src/qt/strayacoin-qt ./bin/linux
+cp -v ./src/qt/test/test_strayacoin-qt ./bin/linux
+cp -v ./strayacoin.conf ./bin/linux/
